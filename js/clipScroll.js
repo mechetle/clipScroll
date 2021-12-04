8d12828acb305e12fe040e8d2307259233550f18
mechetle <pigsky123@gmail.com>
2021-12-05 10:42:40 +1100

    made it so it works in multiple instances. Oh yeah also removed mask from text and separated demo from actual useful css

--- js/clipScroll.js
+++ js/clipScroll.js
@@ -1,28 +1,29 @@
 /*  
 clipScroll
-A really basic scroll mask thingo
+A really basic scroll mask thingo.
+
 Author: Mechetle
 */
 
-let wrapper = document.querySelectorAll(".exec");
+let wrapper = document.querySelectorAll(".cS");
 // todo: update this everytime on window change size
 let heightOfWin = window.innerHeight;
 
 wrapper.forEach(el => {
   // todo: update this everytime on window change size
   let heightOfWrap = el.offsetHeight;
-  
-  let scrollPos = 0;
-  let ticking = false;
+  let rectTop = el.getBoundingClientRect().top;
 
+  let ticking = false;
+  
   function doSomething(scrollPos) {
-    console.log(scrollPos + " / " + (heightOfWrap - heightOfWin));
-    percentage = scrollPos / (heightOfWrap - heightOfWin);
+    console.log((scrollPos - rectTop) + " / " + (heightOfWrap - heightOfWin));
+    percentage = (scrollPos - rectTop) / (heightOfWrap - heightOfWin);
     let val = (1 - percentage) * 100;
     console.log(val);
     
     // apply to the layer
-    let filled = document.querySelector(".exec span:last-of-type");
+    let filled = el.querySelector("span:last-of-type");
     filled.style.clipPath = `inset(${val}% 0 0 0)`;
   }
 
