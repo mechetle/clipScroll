54512f9753c8bad03504cce52b0148e1a1ee4fb0
mechetle <pigsky123@gmail.com>
2021-12-05 13:10:11 +1100

    + responsive

--- js/clipScroll.js
+++ js/clipScroll.js
@@ -5,34 +5,89 @@ A really basic scroll mask thingo.
 Author: Mechetle
 */
 
+let cS = {
+  options: {
+    debug: false,
+  },
+};
+
 let wrapper = document.querySelectorAll(".cS");
-// todo: update this everytime on window change size
 let heightOfWin = window.innerHeight;
+// Update this everytime on window change size
+window.addEventListener('resize', () => {
+  heightOfWin = window.innerHeight;
+});
+
+function pixelToInt(px) {
+  return px.replace("px", "");
+}
 
 wrapper.forEach(el => {
-  // todo: update this everytime on window change size
   let heightOfWrap = el.offsetHeight;
-  let rectTop = el.getBoundingClientRect().top;
+  // Update this everytime on window change size
+  window.addEventListener('resize', () => {
+    heightOfWrap = el.offsetHeight;
+  });
+  let relHeight = heightOfWrap - heightOfWin;
 
+  let rectTop = el.getBoundingClientRect().top + window.scrollY;
   let ticking = false;
   
-  function doSomething(scrollPos) {
-    console.log((scrollPos - rectTop) + " / " + (heightOfWrap - heightOfWin));
-    percentage = (scrollPos - rectTop) / (heightOfWrap - heightOfWin);
-    let val = (1 - percentage) * 100;
-    console.log(val);
-    
-    // apply to the layer
-    let filled = el.querySelector("span:last-of-type");
-    filled.style.clipPath = `inset(${val}% 0 0 0)`;
+  // Getting last span tag:
+  let filled = el.querySelector("span:last-of-type");
+  
+  // declaring variables that will later calc:
+  let percentage, val;
+
+  // get line height of the text, then it will be used in the clip style to fix the gap:
+  let computedStyle = window.getComputedStyle(document.querySelector("h1"));
+  let fontSize = pixelToInt(computedStyle.fontSize);
+  let lineHeight = pixelToInt(computedStyle.lineHeight);
+  let lh_gap, lh_ratio;
+
+  if (lineHeight != fontSize) {
+    lh_gap = fontSize - lineHeight;
+    lh_ratio = fontSize / lineHeight;
+  } else {
+    lh_gap = 0;
+    lh_ratio = 1;
+  }
+  console.log(lh_gap);
+  
+
+  function updateClipping(scrollPos) {
+    // This is so it works for every div or section with it applied on.
+    sectionSrollPos = scrollPos - rectTop;
+    // This is necessary so it stops calculating another el that is not visible on screen
+    if (sectionSrollPos >= 0) {
+      percentage = sectionSrollPos / relHeight;
+      val = (1 - percentage) * 100;
+
+      // When somehow if the user is first spawned in a section of a page previous text will be filled in.
+      if (sectionSrollPos > relHeight) {
+        filled.style.clipPath = `inset(0 0 -${lh_gap}px 0)`;
+      }
+      
+      // only apply when it is visible
+      if (val >= 0) {
+        // apply to the layer:
+        filled.style.clipPath = `inset(calc(${val}% * ${lh_ratio}) 0px -${lh_gap}px 0px)`;
+
+        // If debugging setting is on then show scroll info:
+        if (cS.options.debug) {
+          console.log(sectionSrollPos + " / " + relHeight);
+          console.log(val);
+        }
+      }
+    }
   }
 
   document.addEventListener('scroll', function(e) {
-    lastKnownScrollPosition = window.scrollY;
+    lastKnownScrollPos = window.scrollY;
 
     if (!ticking) {
       window.requestAnimationFrame(function() {
-        doSomething(lastKnownScrollPosition);
+        updateClipping(lastKnownScrollPos);
         ticking = false;
       });
 
